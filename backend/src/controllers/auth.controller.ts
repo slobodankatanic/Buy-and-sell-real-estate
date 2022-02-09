0758015fc97c08a044e8f498ade8a088cbf9ef92
slobodankatanic <slobo.katanic429@gmail.com>
2022-02-09 05:05:22 +0100

    Add register input validation

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -6,7 +6,7 @@ export class AuthController {
         let username = req.body.username;
         let password = req.body.password;
 
-        User.findOne({ 'username': username, 'password': password }, (err, user) => {
+        User.findOne({ 'username': username, 'password': password, "status": "approved" }, (err, user) => {
             if (user) {
                 res.status(200).json(user);
             } else {
@@ -18,27 +18,45 @@ export class AuthController {
     }
 
     register = (req: express.Request, res: express.Response) => {
-        let user = new User({
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            username: req.body.username,
-            password: req.body.password,
-            type: req.body.type,
-            city: req.body.city,
-            dob: req.body.dateOfBirth,
-            telephone: req.body.telephone,
-            email: req.body.email,
-            agencyId: Number(req.body.agencyId),
-            licence: Number(req.body.agencyId),
-            status: "awaiting",
-            image: "http://localhost:4000/images/users/__img__" + req.file.originalname,
-            favorites: []
-        });
+        User.find({ username: req.body.username }, (err, user) => {
+            if (user) {
+                res.json({
+                    'message': 'username exists',
+                    'status': 1
+                });
+            } else {
+                User.find({ email: req.body.email }, (err, user) => {
+                    if (user) {
+                        res.json({
+                            'message': 'email exists',
+                            'status': 2
+                        })
+                    } else {
+                        let user = new User({
+                            firstname: req.body.firstname,
+                            lastname: req.body.lastname,
+                            username: req.body.username,
+                            password: req.body.password,
+                            type: req.body.type,
+                            city: req.body.city,
+                            dob: req.body.dateOfBirth,
+                            telephone: req.body.telephone,
+                            email: req.body.email,
+                            agencyId: Number(req.body.agencyId),
+                            licence: Number(req.body.licence),
+                            status: "awaiting",
+                            image: "http://localhost:4000/images/users/__img__" + req.file.originalname,
+                            favorites: []
+                        });
 
-        user.save().then(user => {
-            res.status(200).json({ 'message': 'user added' });
-        }).catch(err => {
-            res.status(400).json({ 'message': 'error' });
-        })
+                        user.save().then(user => {
+                            res.json({ 'message': 'user added', 'status': 0 });
+                        }).catch(err => {
+                            res.json({ 'message': 'error', status: 3 });
+                        })
+                    }
+                })
+            }
+        });
     }
 }
--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -16,18 +16,25 @@ export class RegisterComponent implements OnInit {
 
   captcha: string;
 
-  firstname: string;
-  lastname: string;
-  username: string;
-  password: string;
+  firstname: string = "";
+  lastname: string = "";
+  username: string = "";
+  password: string = "";
   type: string = "buyer";
-  confirmPassword: string;
-  city: string;
-  dob: Date;
-  email: string;
-  telephone: string;
+  confirmPassword: string = "";
+  city: string = "";
+  dob: string = "";
+  email: string = "";
+  telephone: string = "";
   agency: string = "Choose agency";
   licence: string = "";
+
+  agencyError: string = "";
+  firstnameError: string = "";
+  lastnameError: string = "";
+  usernameError: string = "";
+  passwordError: string = "";
+  confirmPasswordError: string = "";
 
   ngOnInit(): void {
     this.commonService.getAllAgencies().subscribe((agencies: Agency[]) => {
@@ -41,7 +48,7 @@ export class RegisterComponent implements OnInit {
     (<HTMLInputElement>document.getElementById("agency")).disabled = true;
     (<HTMLInputElement>document.getElementById("licence")).disabled = true;
     this.type = "owner";
-    this.agency = "";
+    this.agency = "Choose agency";
     this.licence = "";
   }
 
@@ -55,7 +62,7 @@ export class RegisterComponent implements OnInit {
     (<HTMLInputElement>document.getElementById("agency")).disabled = true;
     (<HTMLInputElement>document.getElementById("licence")).disabled = true;
     this.type = "buyer";
-    this.agency = "";
+    this.agency = "Choose agency";
     this.licence = "";
   }
 
@@ -64,25 +71,83 @@ export class RegisterComponent implements OnInit {
     //   alert(this.dob)
     // }
 
-    if (this.agency == "Choose agency") {
-      this.agency = "";
+    this.agencyError = "";
+    this.firstnameError = "";
+    this.lastnameError = "";
+    this.usernameError = "";
+    this.passwordError = "";
+    this.confirmPasswordError = "";
+
+    let regexName = new RegExp(/^[A-Za-z]{1,}$/);
+
+    if (this.firstname == "") {
+      this.firstnameError = "Required field";
+      return;
+    } else if (!regexName.test(this.firstname)) {
+      this.firstnameError = "Only letters allowed";
+      return;
     }
 
-    this.authService.register(
-      this.firstname,
-      this.lastname,
-      this.username,
-      this.password,
-      this.city,
-      this.dob,
-      this.telephone,
-      this.email,
-      this.type,
-      this.agency,
-      this.licence,
-      this.profileImage).subscribe(resp => {
-        // proveri status poruke - greske: email, username
-      });
+    if (this.lastname == "") {
+      this.lastnameError = "Required field";
+      return;
+    } else if (!regexName.test(this.lastname)) {
+      this.lastnameError = "Only letters allowed";
+      return;
+    }
+
+    if (this.username == "") {
+      this.usernameError = "Required field";
+      return;
+    }
+
+    let regexSpecChars = new RegExp(/[@%+!#$^?:]/);
+    let regexLength = new RegExp(/.{8,}/);
+    let regexUpper = new RegExp(/[A-Z]/);
+    let regexNumber = new RegExp(/[0-9]/);
+    let regexStart = new RegExp(/^[A-Za-z]/);
+
+    if (!regexSpecChars.test(this.password) ||
+        !regexNumber.test(this.password) ||
+        !regexUpper.test(this.password)) {
+      this.passwordError = "Password must contain special character, upper letter and number";
+      return;
+    } else if (!regexLength.test(this.password)) {
+      this.passwordError = "Min. password length is 8";
+      return;
+    } else if (!regexStart.test(this.password)) {
+      this.passwordError = "Password must start with letter";
+    }
+
+    if (this.confirmPassword != this.password) {
+      this.confirmPasswordError = "You must confirm password";
+      return;
+    }
+
+    if (this.agency == "Choose agency") {
+      if (this.type != "agent") {
+        this.agency = "";
+      } else {
+        this.agencyError = "You must choose agency";
+        return;
+      }
+    }
+
+    // this.authService.register(
+    //   this.firstname,
+    //   this.lastname,
+    //   this.username,
+    //   this.password,
+    //   this.city,
+    //   this.dob,
+    //   this.telephone,
+    //   this.email,
+    //   this.type,
+    //   this.agency,
+    //   this.licence,
+    //   this.profileImage).subscribe(resp => {
+    //     // proveri status poruke - greske: email, username
+    //   });
   }
 
   resolved(captchaResponse: string) {
